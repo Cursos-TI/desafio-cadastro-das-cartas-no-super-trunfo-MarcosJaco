c3f31d1e8f1d783df140c7cf7d141cbed696f2f1
MarcosJaco <marcosjaco2005@gmail.com>
2025-01-24 21:15:43 +0000

    inicialização das váriaveis e introdução do jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    int populacao;
+    int area;
+    int pib;
+    int npturismo;
+    char cidadenome1a[100], cidadenome2a[100], cidadenome3a[100],cidadenome4a[100],cidadenome5a[100],cidadenome6a[100],cidadenome7a[100],cidadenome8a[100]
+    char estadonome1a[100], estadonome2a[100], estadonome3a[100], estadonome4a[100];
+    char paisnome1[100], paisnome2[100], paisnome3[100], paisnome4[100];
+
+printf("Bem-Vindos Ao Jogo SuperTrunfo Países\n");
+
+printf("\n");
+
+printf("Para Iniciar Sua Jornada No Jogo,É Necessário Cadastrar Suas Cartas...\n"); //introdução do jogo + explicação
+printf("Cada Carta Representa Um País,Cada País Possui 4 Estados E Cada Estado Possui 8 Cidades.\n");
+printf("Que Vença O Mais Forte!!!\n");
+
+printf("Qual o nome do seu País?\n"); //começo do cadastro das cartas, País de código A
+scanf("%s", paisnome1);
+
+printf("Qual o nome da cidade número 1 do seu País?")
+
+
+    
+printf("\n");
+printf("\n");
+printf("\n");
     return 0;
 }
